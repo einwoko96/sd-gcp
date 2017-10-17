5f826b5cc86d900222f6434d40ae5cd1a9f6d51a
freeman <jhfreeman@utexas.edu>
2017-10-16 20:06:08 -0500

    Added support for: dropout, recurrent_dropout, unit forget bias (lstm), and use of GRU architecture in place of lstm

--- local_run.sh
+++ local_run.sh
@@ -5,6 +5,10 @@ export JOB_NAME="lstm_train_$(date +%Y%m%d_%H%M%S)"
 python2 trainer/train.py \
 	--job_type local \
 	--job_dir $(pwd) \
-	--data_dir 40 \
-	--seq_length 40 \
+	--model_structure gru \
+	--data_dir ucf-5-npy \
+	--seq_length 5 \
 	--batch_size 32 \
+	--dropout 0.5 \
+	--recurrent_dropout 0.0 \
+	--unit_forget_bias False
