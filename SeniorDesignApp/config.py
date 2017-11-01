02f713b53ce39d647707bb36eab2244838ef5469
Eyenunwana Nwoko <einwoko96@yahoo.com>
2017-11-01 14:14:28 -0500

    Timeout on request increased

--- SeniorDesignApp/config.py
+++ SeniorDesignApp/config.py
@@ -1,4 +1,5 @@
 PROJECT_ID = 'sd-lstm'
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'mp4', 'avi'])
-PREDICTION_SERVICE_URL = 'http://130.211.168.102:80'
+# PREDICTION_SERVICE_URL = 'http://70.114.202.148:8080'
+PREDICTION_SERVICE_URL = 'http://0.0.0.0:5000'
--- SeniorDesignApp/sd_lstm.py
+++ SeniorDesignApp/sd_lstm.py
@@ -6,10 +6,13 @@ import base64
 import config
 import logging
 import storage
+import urllib
 import urllib2
+import requests
 from flask import current_app, Flask, render_template, request
 from google.appengine.api import images
 from google.appengine.ext import blobstore
+# from urlgrabber.keepalive import HTTPHandler
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -32,19 +35,29 @@ def upload_video_file(stream, filename, content_type):
     bucket = bucket_filepath[bucket_filepath.find("/")+1:bucket_filepath.rfind("/")]
     filename = bucket_filepath[bucket_filepath.rfind("/")+1:]
 
-    file_url = "https://" + bucket + ".storage.googleapis.com/" + filename
+    file_url = "https://" + "sd-lstm.appspot.com" + ".storage.googleapis.com/" + filename
     return file_url
 
-def fetch_predictions(vid_url):
+def fetch_predictions(vid_url, f):
     predictions = {}
+    username = 'dev'
+    password = 'dogclock'
     server_url = current_app.config['PREDICTION_SERVICE_URL']
-    req = urllib2.Request(server_url, json.dumps({'data': vid_stream}),
-                          {'Content-Type': 'application/json'})
-    try:
-        f = urllib2.urlopen(req)
-        predictions = json.loads(f.read())
-    except urllib2.HTTPError as e:
-        logging.exception(e)
+    values = { 'username': username, 'password': password, 'data': vid_url, 'file_name': f}
+    print(f)
+    data = json.dumps(values)
+    # req = urllib2.Request(server_url, data, {'Content-Type': 'application/json'})
+    # keepalive_handler = HTTPHandler()
+    # opener = urllib2.build_opener(keepalive_handler)
+    # urllib2.install_opener(opener)
+    req = requests.get(server_url, data=data, headers={'Content-Type': 'application/json'}, timeout=120)
+    predictions = req.json()
+
+    # try:
+    #     f = urllib2.urlopen(req)
+    #     predictions = json.loads(f.read())
+    # except urllib2.HTTPError as e:
+    #     logging.exception(e)
 
     logging.info('Predictions: %s', predictions)
 
@@ -62,7 +75,7 @@ def index():
 def video():
     return render_template('video.html')
 
-@app.route('/video', methods=['POST'])
+@app.route('/video', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
         vid = request.files.get('video')
@@ -72,19 +85,23 @@ def predict():
         content_type = vid.content_type
         video_url = upload_video_file(vid_stream, filename, content_type)
 
-        # predictions = fetch_predictions(vid_url=video_url)
+        predictions = fetch_predictions(vid_url=video_url, f=filename)
 
-        # top1 = predictions["label1"]
+        try:
+            top1 = predictions["label1"]
 
-        # top2 = predictions["label2"]
+            top2 = predictions["label2"]
 
-        # top3 = predictions["label3"]
+            top3 = predictions["label3"]
 
-        # top4 = predictions["label4"]
+            top4 = predictions["label4"]
 
-        # top5 = predictions["label5"]
+            top5 = predictions["label5"]
 
-        return render_template('video.html', video_url=video_url, file_name=video_url)#, file_name=filename, one=top1, two=top2, three=top3, four=top4, five=top5)
+            return render_template('video.html', video_url=video_url, file_name=filename, one=top1, two=top2, three=top3, four=top4, five=top5)
+        except Exception:
+
+            return render_template('video.html', video_url=video_url)
 
 @app.errorhandler(500)
 def server_error(e):
